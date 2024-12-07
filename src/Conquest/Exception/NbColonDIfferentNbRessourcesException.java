0a5ac17916bfdad740e32ba660b5e085331bfd37
Okdak-Sempai <devasdamien@gmail.com>
2024-12-07 13:29:25 +0100

    Reparer et tester les algo c'est tout

---
+++ src/Conquest/Exception/NbColonDIfferentNbRessourcesException.java
@@ -0,0 +1,9 @@
+package Conquest.Exception;
+
+public class NbColonDIfferentNbRessourcesException extends Exception {
+
+	public NbColonDIfferentNbRessourcesException(String message) {
+		super(message);
+	}
+
+}
